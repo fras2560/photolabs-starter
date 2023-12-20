177a22d1cbb5f4e7bb11d77d9d9ef2955be9454d
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-12-19 21:34:51 -0500

    Show three photos for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,16 @@ const sampleDataForPhotoListItem = {
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
-
+const photos = [{...sampleDataForPhotoListItem, id: "1"}, {...sampleDataForPhotoListItem, id: "2"}, {...sampleDataForPhotoListItem, id: "3"}].map((photo) => {
+  return (
+    <PhotoListItem key={photo.id} {...photo} />
+  );
+});
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem key={sampleDataForPhotoListItem.id} {...sampleDataForPhotoListItem} />
+      {photos}
     </div>
   );
 };
