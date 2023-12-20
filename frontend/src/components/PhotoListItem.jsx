3be3db7ba4fbe64ec1161ae7d5302505dc6d5e1a
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-12-19 21:30:23 -0500

    Add class name for photo list item
    Apply the styling using BEM class names

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,15 +5,20 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div className="PhotoList">
-      <img src={props.imageSource}></img>
-      <img src={props.profile}></img>
-      <h1>
-        {props.username}
-      </h1>
-      <h2>
-        {props.location.city}, {props.location.country}
-      </h2>
+    <div className="photo-list__item">
+      <img src={props.imageSource} className="photo-list__image"></img>
+      <div className="photo-list__user-details">
+        <img src={props.profile} className="photo-list__user-profile"></img>
+        <div className="photo-list__user-info">
+          {props.username}
+          <div className="photo-list__user-location">
+            {props.location.city}, {props.location.country}
+          </div>
+        </div>
+        
+      </div>
+
+
     </div>
   );
 };
