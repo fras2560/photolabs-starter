b0c0b7723c65cdd14741a369f29f821f500c5a35
fras2560 <dallas.fraser.waterloo@gmail.com>
2023-12-19 21:42:41 -0500

    Implement the favorite button
    Add it to photo list item

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,16 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+  const [favorite, setFavorite] = useState(false);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={() => setFavorite(!favorite)}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={favorite}></FavIcon>
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
+      <PhotoFavButton></PhotoFavButton>
       <img src={props.imageSource} className="photo-list__image"></img>
       <div className="photo-list__user-details">
         <img src={props.profile} className="photo-list__user-profile"></img>
